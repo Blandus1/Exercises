016b86f997d7cf7207c90395b0728bf3b1efc492
Blandus1 <blandine0045@gmail.com>
2025-07-04 12:26:29 -0700

    first project

--- beginner-projects/src/Todo.jsx
+++ beginner-projects/src/Todo.jsx
@@ -4,40 +4,44 @@ function Todo (){
 
     const [input,setInput] = useState('');
     const [additems,setAdditems] = useState([])
-    //const [list, setList] = useState('')
-    const [complete,setComplete]= useState(true)
-const [deletevalue,setDeletevalue]= useState([])
-const [count,setCount] = useState(0)
+    // const [complete,setComplete]= useState(false)
+
+
 
     function handleChange({target}){
      setInput(target.value)
     }
 
     function handleClick(e){
- e.preventDefault()
-        setAdditems((prev)=> [...prev,input ])// this gives the current value followed by the previous value 
+         e.preventDefault()
+        setAdditems((prev)=> [...prev,{text:input,complete: false} ])// this gives the current value followed by the previous value 
     }
 
-    const handleDeletevalue=(deleted)=>{
-       setAdditems(additems.filter((_,b)=> b !== deleted))
+    const handleDeletevalue=(index)=>{
+       setAdditems(additems.filter((_,b)=> b !== index))
     }
+ 
+const handleComplete = (index) =>{
+     setAdditems(prev =>prev.map((a, b) =>b === index ? { ...a, complete: !a.complete } : a));
+     //this line juggles btn complete being true or false, where when the item was true
+     //  if it is true it turns false and vice versa 
+     // the map method helps us to have the change on the clicked index(individually)
 
-    const handleCount= ()=>{
-        setCount(count + 1)
-    }
+}
+    
 
     return(
         <>
         <h2>Project1: Personal To-do list</h2>
-        <p>task count:{count}</p>
+         <p>Task count:{additems.length}</p> {/*this sets the initial count to zero and  does the counting at every button click  */}
         <input value={input} onChange={handleChange} ></input>
-        <button onClick={()=> {handleClick(); handleCount()}} > Add to list </button>
-        {/* <button onClick = {()=> setCount(count +1)} > {count}</button> */}
+        <button onClick={handleClick} > Add to list </button>
+      
         
         <ol>
         
                 {additems.map((a,b)=>{
-                     return <li key={b}>{a}<button onClick={()=> setComplete(!complete)}> {complete? "complete": "incomplete"}</button>
+                      return <li key={b}>{a.text}<button onClick={()=> handleComplete(b)}> {a.complete? "complete": "incomplete"}</button>
                                            <button onClick={() => handleDeletevalue(b)}>Delete</button>
                      </li>
                      })}
